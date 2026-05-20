a54a0ed8dde46479878b95792c7f44707bef9a4b
merajul2027 <merajulhaqueqa@gmail.com>
2026-05-20 07:44:36 +0530

    Add Task_2.js - Grade calculation based on marks

---
+++ Task_2.js
@@ -0,0 +1,22 @@
+let marks=75;
+if(marks>=90)
+{
+    console.log("Grade is A");
+}
+if(marks>=80 && marks<90)
+{
+    console.log("Grade is B");
+}
+
+if(marks>=70 && marks<80)
+{
+    console.log("Grade is C");
+}
+if(marks>=60 && marks<70)
+{
+    console.log("Grade is D");
+}
+if(marks<60)
+{
+    console.log("Grade is F");
+}
